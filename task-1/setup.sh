3ad70e95da6ea661d0dfd311048581458998c3d3
mohammedalfakih-dev <mohammedalfakih23@gmail.com>
2026-01-14 10:04:36 +0100

    submitting task 1 assignment

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,35 @@
-# Write your code here
+#!/usr/bin/env bash
+set -e
+echo "creating project.."
+mkdir project
+cd project
+git init
+mkdir resources
+mkdir src
+mkdir src/database
+mkdir src/profile
+touch README.md
+touch settings.conf
+touch resources/family\ picture.jpg
+touch resources/icon.png
+touch resources/logo.png
+touch src/program.java
+git add .
+git commit -m "Initial commit"
+sleep 3
+echo "setup project..."
+echo "Welcome to my project" > README.md
+rmdir src/profile
+rm resources/family\ picture.jpg
+git add .
+git commit -m "update readme and remove profile and family picture"
+sleep 3
+ls resources
+echo "setup javascript"
+mv src/program.java src/program.js
+echo 'console.log("JavaScript works!");' > src/program.js
+node src/program.js
+git add .
+git commit -m "convert program to JavaScript"
+ls ~
+echo "All done!"
